41b6a4a841d632dfdecc76fa3b52630b1c8cf6ae
GilWoo Lee <38275652+dlrfdnwkd@users.noreply.github.com>
2023-07-30 17:44:47 +0900

    Add problem 12913-땅따먹기 * Add 땅따먹기.java
    * Add README.md

---
+++ Algorithm/Problems/프로그래머스/lv2/12913-땅따먹기/땅따먹기.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    int solution(int[][] land) {
+        int answer = 0;
+
+        if(land.length > 1) {
+            for(int i = 1;i < land.length;i++) {
+                land[i][0] = land[i][0] + max(land[i - 1][1],land[i - 1][2],land[i - 1][3]);
+                land[i][1] = land[i][1] + max(land[i - 1][0],land[i - 1][2],land[i - 1][3]);
+                land[i][2] = land[i][2] + max(land[i - 1][0],land[i - 1][1],land[i - 1][3]);
+                land[i][3] = land[i][3] + max(land[i - 1][0],land[i - 1][1],land[i - 1][2]);
+            }
+        }
+        
+        for(int i = 0;i < 4;i++) {
+            answer = Math.max(answer,land[land.length - 1][i]);
+        }
+
+        return answer;
+    }
+    
+    public int max(int a,int b,int c) {
+        return (a > b) && (a > c) ? a : (b > c) ? b : c;
+    }
+    
+}
