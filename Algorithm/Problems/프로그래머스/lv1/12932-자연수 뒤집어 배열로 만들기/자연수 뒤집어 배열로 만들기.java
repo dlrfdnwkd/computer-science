cee1e73fe6a1145d85f61e7bcb7343d59d566960
GilWoo Lee <38275652+dlrfdnwkd@users.noreply.github.com>
2024-06-15 09:26:48 +0900

    Add problem 12932-자연수 뒤집어 배열로 만들기 * Add 자연수 뒤집어 배열로 만들기.java
    * Add README.md

---
+++ Algorithm/Problems/프로그래머스/lv1/12932-자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int[] solution(long n) {
+        return new StringBuilder(String.valueOf(n))
+                .reverse()
+                .toString()
+                .chars()
+                .map(Character::getNumericValue)
+                .toArray();
+    }
+}
