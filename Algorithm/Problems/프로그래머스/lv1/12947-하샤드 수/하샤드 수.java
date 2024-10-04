3db95111aad3c7ea463a5cc7376de9098603c7bd
GilWoo Lee <38275652+dlrfdnwkd@users.noreply.github.com>
2024-10-04 18:04:51 +0900

    Add problem 12947-하샤드 수 * Add 하샤드 수.java
    * Add README.md

---
+++ Algorithm/Problems/프로그래머스/lv1/12947-하샤드 수/하샤드 수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = String.valueOf(x)
+                        .chars()
+                        .map(Character::getNumericValue)
+                        .sum();
+        return x % sum == 0;
+    }
+}
