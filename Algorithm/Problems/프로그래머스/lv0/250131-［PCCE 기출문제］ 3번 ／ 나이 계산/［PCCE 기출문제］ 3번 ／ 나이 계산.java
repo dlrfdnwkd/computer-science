e76d42d36b8ed105e238277ae58d5993060fd312
GilWoo Lee <38275652+dlrfdnwkd@users.noreply.github.com>
2025-02-18 23:46:11 +0900

    Add problem 250131-[PCCE 기출문제] 3번 / 나이 계산 * Add [PCCE 기출문제] 3번 / 나이 계산.java
    * Add README.md

---
+++ Algorithm/Problems/프로그래머스/lv0/250131-［PCCE 기출문제］ 3번 ／ 나이 계산/［PCCE 기출문제］ 3번 ／ 나이 계산.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        String age_type = sc.next();
+        int answer = 0;
+
+        if (age_type.equals()) {
+            answer = ;
+        }
+        else if (age_type.equals("Year")) {
+            ;
+        }
+
+        System.out.println(answer);
+    }
+}
