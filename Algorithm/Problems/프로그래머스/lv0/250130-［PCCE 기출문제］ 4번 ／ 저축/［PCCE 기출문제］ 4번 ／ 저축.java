a02688cedc09d71b57718d91be0a8c2464063a05
GilWoo Lee <38275652+dlrfdnwkd@users.noreply.github.com>
2025-02-19 00:35:29 +0900

    Add problem 250130-[PCCE 기출문제] 4번 / 저축 * Add [PCCE 기출문제] 4번 / 저축.java
    * Add README.md

---
+++ Algorithm/Problems/프로그래머스/lv0/250130-［PCCE 기출문제］ 4번 ／ 저축/［PCCE 기출문제］ 4번 ／ 저축.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int before = sc.nextInt();
+        int after = sc.nextInt();
+
+        int money = start;
+        int month = 1;
+        while (money < 70) {
+            money += ;
+            month++;
+        }
+        while ( < 100) {
+            ;
+            month++;
+        }
+
+        System.out.println(month);
+    }
+}
