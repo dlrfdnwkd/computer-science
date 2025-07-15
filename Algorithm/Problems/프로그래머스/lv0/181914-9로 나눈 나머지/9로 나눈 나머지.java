896a1b8a7ddd4aaf3e2fa0c5d038cb572fd38077
GilWoo Lee <38275652+dlrfdnwkd@users.noreply.github.com>
2025-07-15 20:13:19 +0900

    Add problem 181914-9로 나눈 나머지 * Add 9로 나눈 나머지.java
    * Add README.md

---
+++ Algorithm/Problems/프로그래머스/lv0/181914-9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String number) {
+        int n = 0;
+
+        for (char c : number.toCharArray()) {
+            n += c - '0';
+        }
+
+        return n % 9;
+    }
+}
