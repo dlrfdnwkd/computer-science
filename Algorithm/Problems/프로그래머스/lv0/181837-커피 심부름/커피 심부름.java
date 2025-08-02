5c704c1cb13c19ca7b064b20d8573049bc24d7ed
GilWoo Lee <38275652+dlrfdnwkd@users.noreply.github.com>
2025-08-03 00:42:32 +0900

    Add problem 181837-커피 심부름 * Add 커피 심부름.java
    * Add README.md

---
+++ Algorithm/Problems/프로그래머스/lv0/181837-커피 심부름/커피 심부름.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+
+        for (String coffee : order) {
+            answer += coffee.contains("cafelatte") ? 5000 : 4500;
+        }
+
+        return answer;
+    }
+}
